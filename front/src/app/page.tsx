0ce2abfa1d39c2e86c4167617f8d9c6e4536924e
uxelf <uxself@gmail.com>
2025-12-03 16:30:05 +0100

    first commit

---
+++ front/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-6xl font-bold underline text-amber-500">
+      Hello world!
+    </h1>
+  )
+}
